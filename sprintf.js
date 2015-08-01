ca18d765fd7b7ca8e8314533ea03ea35f64f9e70
Ralf S. Engelschall <rse@engelschall.com>
2015-08-01 19:04:33 +0200

    update dependencies

--- sprintf.js
+++ sprintf.js
@@ -267,7 +267,7 @@
     };
 
     /*  brand with the version  */
-    sprintf.version = { text: "1.1.9", major: 1, minor: 1, micro: 9 };
+    sprintf.version = { text: "1.2.0", major: 1, minor: 2, micro: 0 };
 
     /*  export API  */
     return sprintf;
